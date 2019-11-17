e40366e9cda1a2cc8e73cdedb993473d671feb74
Turi <manfredo.mugheddu@gmail.com>
2019-11-17 12:18:16 +0000

    need to study javascript for developing dropdown menus

--- resources/land.js
+++ resources/land.js
@@ -1,3 +1,58 @@
+/* search dropdown */
+
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+/* bar dropdown
+
+When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+$(document).click(function(){
+  $("#dropdown").hide();
+})
+
+
+
+function myFunction2() {
+  document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn2')) {
+  var myDropdown = document.getElementById("myDropdown2");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+
+
+
+
+
+
+/* top location dropdown */
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
@@ -12,3 +67,4 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
